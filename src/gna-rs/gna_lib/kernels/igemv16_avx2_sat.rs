ef4a418a9b39c42b3479cf42fb26e848fafac3f5
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2026-04-20 01:30:08 +0900

    rename

--- src/gna-rs/gna_lib/kernels/igemv16_avx2_sat.rs
+++
@@ -1,9 +0,0 @@
-/**
- @copyright Copyright (C) 2020-2022 Intel Corporation
- SPDX-License-Identifier: LGPL-2.1-or-later
-*/
-// Auto-generated Rust stub for original: gna/src/gna-lib/kernels/igemv16_avx2-sat.cpp
-
-#[allow(dead_code)]
-
-pub fn igemv16_avx2_sat() {}
