813145d8b6992372db6d99e1344c1c6c294b6352
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2026-04-20 01:07:28 +0900

    move

--- src/gna-rs/src/gna_lib/model_error.rs
+++
@@ -1,8 +0,0 @@
-/**
- @copyright Copyright (C) 2020-2022 Intel Corporation
- SPDX-License-Identifier: LGPL-2.1-or-later
-*/
-/// Skeleton for ModelError and helpers.
-
-#[derive(Debug)]
-pub struct ModelError;
--- src/gna-rs/src/gna_lib/string_helper.rs
+++
@@ -1,9 +0,0 @@
-/**
- @copyright Copyright (C) 2020-2022 Intel Corporation
- SPDX-License-Identifier: LGPL-2.1-or-later
-*/
-
-/// Stub for StringHelper
-
-#[allow(dead_code)]
-pub fn trim(_s: &str) -> &str { _s }
